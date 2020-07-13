c78d126ace954a2c80beb2543f47925b4ab48fa7
cficht <chris.ficht@gmail.com>
2020-07-13 15:30:08 -0700

    added eslint and next config

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>MAIN</h1>
+      </main>
+
+      <footer>
+        <h1>FOOTER</h1>
+      </footer>
+    </div>
+  );
+}
