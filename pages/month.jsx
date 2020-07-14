bc4687b215ab934a4961fd5105c916f7cf232f0e
cficht <chris.ficht@gmail.com>
2020-07-14 12:01:59 -0700

    mapping through days on month page

--- pages/month.jsx
+++ pages/month.jsx
@@ -19,12 +19,21 @@ export default function month() {
     // REFACTOR
     const monthDays = new Array(moment((months + 1).toString()).daysInMonth());
     for (let i = 0; i < monthDays.length; i++) monthDays[i] = moment().year(years).month(months).date(i + 1).format("dddd, MMMM Do YYYY");
-    
+
+    // USE THIS TO GET BEFORE AND AFTER CALENDAR DAYS
+    console.log(moment().day(monthDays[0]).format('d'));
+    console.log(moment().day(monthDays[monthDays.length - 1]).format('d'));
+
     setDisplayDays({
       month: moment().month(months).format('MMMM'),
       days: monthDays,
     });
   }, [targetDate]);
+
+  const dayNodes = displayDays.days?.map(day => {return (
+  <div className="month-day" key={day}>
+    <h3>{day}</h3>
+  </div>)})
 
   return (
     <div>
@@ -38,7 +47,7 @@ export default function month() {
             <h3>{displayDays.month ? displayDays.month : ''}</h3>
           </div>
           <div className="month-body">
-            Eventual Days
+            {dayNodes}
           </div>
         </div>
       </main>
