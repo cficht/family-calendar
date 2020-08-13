ebbf6baf91c50f7475c7d6c5a480b1345a1d4484
cficht <chris.ficht@gmail.com>
2020-08-13 12:30:17 -0700

    custom favicon

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -31,6 +31,11 @@ export default function admin() {
         <>
           <Head>
             <title>Family Calendar: Admin View</title>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="manifest" href="/site.webmanifest" />
           </Head>
           <main className="page-container">
             <div className="page-header">
--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -30,6 +30,11 @@ export default function calendar() {
         <>
           <Head>
             <title>Family Calendar: {view} View</title>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="manifest" href="/site.webmanifest" />
           </Head>
           <main className="page-container"> 
             <div className="page-header">
